263b0f8b7c59751df9af40d0166b2ebfe3af97ef
Matt Whiteside <matt.whiteside@gmail.com>
2014-08-07 10:27:44 -0700

    Initial Commit

---
+++ Combinatorics/Combinatorics.h
@@ -0,0 +1,19 @@
+//
+//  Combinatorics.h
+//  Combinatorics
+//
+//  Created by Matthew Whiteside on 8/7/14.
+//  Copyright (c) 2014 mattwhiteside. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Combinatorics.
+FOUNDATION_EXPORT double CombinatoricsVersionNumber;
+
+//! Project version string for Combinatorics.
+FOUNDATION_EXPORT const unsigned char CombinatoricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Combinatorics/PublicHeader.h>
+
+
